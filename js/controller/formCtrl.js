0bdd1d86fdf4bf2776e31d86cd9d622366c82e7c
Edirlei <edirleipizatto@ciss.com.br>
2016-03-15 18:22:54 -0300

    segundo commit

---
+++ js/controller/formCtrl.js
@@ -0,0 +1,12 @@
+myApp.controller('FormCtrl', function($scope) {
+	
+	$scope.pessoa = {
+		name : 'Edirlei',
+		lastname : 'Pizatto',
+		age : 33,
+		phone : '(46)8822-8153',
+		email : 'edirleipizatto@gmail.com'
+	}
+
+});
+

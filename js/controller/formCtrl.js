5264d214d91536da9c11f61fe6e08f45ae753d50
Edirlei <edirleipizatto@ciss.com.br>
2016-03-15 21:03:15 -0300

    angular dia 15/03

--- js/controller/formCtrl.js
+++ js/controller/formCtrl.js
@@ -1,4 +1,8 @@
 myApp.controller('FormCtrl', function($scope) {
+
+	$scope.indexTarefa = 0;
+
+	$scope.clicouEditar = false;
 	
 	$scope.pessoa = {
 		name : 'Edirlei',
@@ -7,6 +11,67 @@ myApp.controller('FormCtrl', function($scope) {
 		phone : '(46)8822-8153',
 		email : 'edirleipizatto@gmail.com'
 	}
+
+	$scope.list = [
+		{
+			nomeTarefa : "Lavar o banheiro",
+			dataTarefa : '2016-03-15T02:00:00.123Z'
+		},
+
+		
+		{
+			nomeTarefa : "Jogar o Lixo",
+			dataTarefa : '2016-03-15T10:00:00.123Z'
+		},
+
+		{
+			nomeTarefa : "Passar Roupa",
+			dataTarefa : '2016-03-15T10:00:00.123Z'
+		}
+	];
+	
+	//função para cadastrar uma nova tarefa 
+	$scope.cadastraTarefa = function(data) {
+		if (!!data) {
+
+			$scope.list.push({
+				nomeTarefa : data,
+				dataTarefa : new Date().getTime()
+			});		
+			$scope.tarefa = undefined;
+
+		} 
+		else {
+
+			alert('Informe uma Tarefa.');
+
+		}		
+	};
+
+	//função para editar uma tarefa
+	$scope.editaTarefa = function(dataIndex) {
+		$scope.clicouEditar = true;
+		$scope.indexTarefa = dataIndex;
+		$scope.renomear = $scope.list[dataIndex].nomeTarefa;	
+
+	};
+
+	//função para renomear uma tarefa
+	$scope.renomearTarefa = function(data) {
+		/*var editObj = {};
+		editObj = {
+			'nomeTarefa' : data,
+			'dataTarefa' : $scope.list[$scope.indexTarefa].dataTarefa
+		};
+		console.log(editObj);
+		$scope.list[$scope.indexTarefa] = editObj;*/
+		
+		//console.log(editObj);
+		$scope.list[$scope.indexTarefa].nomeTarefa = data;
+
+		$scope.clicouEditar = false;
+	};
+
 
 });
 
